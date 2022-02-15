e5ff71ea0e4d88eb26d4ac38a5f8e442cf0a642b
Zechariah Burrus <zechburrus@gmail.com>
2022-02-15 15:25:48 -0600

    Part 1 finished

---
+++ Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour {
+    private Vector3 startPosition;
+    private float repeatWidth;
+
+    // Start is called before the first frame update
+    void Start() {
+        //Saves the starting position of the background
+        startPosition = transform.position;
+
+        //Set's the repeat width to half the width of the backgrounds box collider
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //If the background is further to the left than repeatWidth we reset it to it's starting position
+        if(transform.position.x <  startPosition.x - repeatWidth) {
+            transform.position = startPosition;
+        }
+    }
+}
---
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPoisition = new Vector3(25, 0, 0);
+
+    private float startDelay = 2;
+    private float repeatDelay = 2;
+
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start() {
+        playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+
+        InvokeRepeating("SpawnObstacle", startDelay, repeatDelay);
+    }
+
+    void SpawnObstacle() {
+        if(playerControllerScript.gameOver == false) {
+            Instantiate(obstaclePrefab, spawnPoisition, obstaclePrefab.transform.rotation);
+        }
+    }
+}
